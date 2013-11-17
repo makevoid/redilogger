08051504240ff8bf373e9e1c3fee2187a67ca940
Francesco Canessa <makevoid@gmail.com>
2013-11-17 13:45:07 +0100

    get info in parameters

--- redilogger.rb
+++ redilogger.rb
@@ -2,6 +2,7 @@ file = File.read( File.expand_path "~/.textmagic_sms" ).strip
 textmagic_secret, recipient = file.split "|"
 TEXTMAGIC_SECRET = textmagic_secret
 RECIPIENT = recipient
+TARGET_URL = "http://example.com/success"
 
 def log(data)
   gateway = TextMagic::API.new 'makevoid', TEXTMAGIC_SECRET
@@ -14,18 +15,9 @@ require 'bundler'
 Bundler.require :default
 
 class RediLogger < Sinatra::Application
-  get "/trovit/cuoco" do
-    log "#{request.ip} #{request.user_agent[0..120]}"
-    redirect "http://lavoro.trovit.it/index.php/cod.search_jobs/what_d.cuoco/"
-  end
-
-  get "/ebay/cuoco" do
-    log "#{request.ip} #{request.user_agent[0..120]}"
-    redirect "http://annunci.ebay.it/offerte-di-lavoro/offerta/cuochi-e-baristi-offerte/cuoco/"
-  end
 
   get "/*" do
-    log "#{request.ip} #{request.user_agent[0..120]}"
-    redirect "http://annunci.ebay.it/offerte-di-lavoro/offerta/cuochi-e-baristi-offerte/cuoco/"
+    log "RediLogger: #{params.inspect}"
+    redirect TARGET_URL
   end
 end
